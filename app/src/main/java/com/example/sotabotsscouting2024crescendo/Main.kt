7b21fbe2539e40e91a39e1f31a157f7232c01af1
Riley Weaver <riley.weaver2006@gmail.com>
2023-11-21 17:39:30 -0800

    initial commit

---
+++ app/src/main/java/com/example/sotabotsscouting2024crescendo/Main.kt
@@ -0,0 +1,30 @@
+package com.example.sotabotsscouting2024crescendo
+
+
+import android.R
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+//import com.example.SOTAbotsscouting2024crescendo.R.*
+//import com.google.firebase.database.FirebaseDatabase
+//import com.google.firebase.database.ktx.database
+//import com.google.firebase.ktx.Firebase
+import java.util.Stack
+
+class Main : Activity() {
+
+}
